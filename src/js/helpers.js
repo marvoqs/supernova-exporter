a2e72c11ce94ee6e38d11194e879f5fe5cc5c16b
Marek Schwarz <marvoqs@gmail.com>
2022-11-28 12:25:50 +0100

    Customize exporter

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,3 +1,7 @@
+Pulsar.registerFunction("getRemFromPx", function (measure) {
+  return measure / 16;
+});
+
 /**
  * Convert group name, token name and possible prefix into camelCased string, joining everything together
  */
@@ -36,57 +40,114 @@ Pulsar.registerFunction(
 );
 
 /**
+ * Convert group name and possible prefix into camelCased string, joining everything together
+ */
+Pulsar.registerFunction("shortVariableName", function (token, prefix) {
+  // Separate camelCase into words so we don't lose it
+  const separatedCamelCase = token.name.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  // Create array with all path segments and token name at the end
+  const segments = [separatedCamelCase];
+
+  if (prefix && prefix.length > 0) {
+    segments.unshift(prefix);
+  }
+
+  // Create "sentence" separated by spaces so we can camelcase it all
+  let sentence = segments.join(" ");
+
+  // Return camelcased string from all segments
+  sentence = sentence
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+
+  // only allow letters, digits, underscore
+  sentence = sentence.replace(/[^a-zA-Z0-9_]/g, "_");
+
+  // prepend underscore if it starts with digit
+  if (/^\d/.test(sentence)) {
+    sentence = "_" + sentence;
+  }
+
+  return sentence;
+});
+
+/**
  * Behavior configuration of the exporter
  * Prefixes: Add prefix for each category of the tokens. For example, all colors can start with "color, if needed"
  */
 const BEHAVIOR = {
   color: {
     fileName: "colors", // this should be somehow synced with output.json contents
-    varName: "Colors",
+    varName: "colors",
+    itemTypeName: "ColorTokenType",
+    typeName: "ColorsType",
     themeProperty: "colors",
+    tokenType: "Color",
     tokenPrefix: "",
   },
   border: {
     fileName: "borders", // this should be somehow synced with output.json contents
-    varName: "Borders",
+    varName: "borders",
+    itemTypeName: "BorderTokenType",
+    typeName: "BordersType",
     themeProperty: "borders",
+    tokenType: "Border",
     tokenPrefix: "",
   },
   gradient: {
     fileName: "gradients", // this should be somehow synced with output.json contents
-    varName: "Gradients",
+    varName: "gradients",
+    itemTypeName: "GradientTokenType",
+    typeName: "GradientsType",
     themeProperty: "gradients",
+    tokenType: "Gradient",
     tokenPrefix: "",
   },
   measure: {
     fileName: "measures", // this should be somehow synced with output.json contents
-    varName: "Measures",
+    varName: "measures",
+    itemTypeName: "MeasureTokenType",
+    typeName: "MeasuresType",
     themeProperty: "measures",
+    tokenType: "Measure",
     tokenPrefix: "",
   },
 
   shadow: {
     fileName: "shadows", // this should be somehow synced with output.json contents
-    varName: "Shadows",
+    varName: "shadows",
+    itemTypeName: "ShadowTokenType",
+    typeName: "ShadowsType",
     themeProperty: "shadows",
+    tokenType: "Shadow",
     tokenPrefix: "",
   },
   typography: {
-    fileName: "typography", // this should be somehow synced with output.json contents
-    varName: "Typographies",
+    fileName: "typographies", // this should be somehow synced with output.json contents
+    varName: "typographies",
+    itemTypeName: "TypographyTokenType",
+    typeName: "TypographiesType",
     themeProperty: "typographies",
+    tokenType: "Typography",
     tokenPrefix: "",
   },
   radius: {
     fileName: "radii", // this should be somehow synced with output.json contents
-    varName: "Raddii",
+    varName: "raddii",
+    itemTypeName: "RadiusTokenType",
+    typeName: "RadiiType",
     themeProperty: "radii",
+    tokenType: "Radius",
     tokenPrefix: "",
   },
   unknown: {
     fileName: "uknown",
-    varName: "Unknowns",
+    varName: "unknowns",
+    itemTypeName: "any",
+    typeName: "UnknownsType",
     themeProperty: "unknowns",
+    tokenType: "Unknown",
     tokenPrefix: "",
   },
 };
